4dd78da2b3e16eb9304fb41b8272b0a730388e5b
Taleh Rzayev <taleh.rzayev.i@asoiu.edu.az>
2023-03-28 11:42:08 +0400

    seo - Head Jsx

---
+++ app/head.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RootHead() {
+  return (
+    <>
+    <title>Root Head Title</title>
+    </>
+  )
+}
---
+++ app/test/head.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function head() {
+  return (
+    <>
+      <title>Test Head</title>
+    </>
+  );
+}
+ 
